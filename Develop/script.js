11707f285bd226bbdd82abb6191e3dbd75815d13
Wendy Wilgus <96785281+wendywilgus@users.noreply.github.com>
2022-09-15 17:28:51 -0400

    added documentation to JS

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@
 //var generateBtn stores locations
 //document.querySelector will return the first element in the document that matches #generate
 var generateBtn = document.querySelector("#generate");
+//need to connect this to the HTML.  Looking for #generate
 
 var numericalValue = "0123456789".split('');
 var lowerCase  = "abcdefghijklmnopqrstuvwxyz".split('');
@@ -18,6 +19,7 @@ var masterArray = [];
 function generatePassword() {
   var passwordLength = window.prompt("Welcome! Please answer the following questions. How many characters would you like in your password? Password must contain at least 8 characters, but no more than 128 characters.");
   // console.log(passwordLength);
+  //add must return a number
 
   var wantsLowercase = window.confirm("Do you want lowercase letters?");
   console.log(wantsLowercase);
@@ -28,21 +30,29 @@ function generatePassword() {
   if(wantsLowercase){
     masterArray  = masterArray.concat(lowerCase);
     console.log(masterArray);
+    
   }
   if(wantsUppercase){
     masterArray = masterArray.concat(upperCase);
     console.log(masterArray);
+    
   }
   if(wantsNumerical){
     masterArray = masterArray.concat(numericalValue);
     console.log(masterArray);
+    
   }
 
   if(wantsSpecial){
     masterArray = masterArray.concat(specialCharacter);
     console.log(masterArray);
+    
   }
 }
+//Now to create master function to pull randomly from the masterArray. Parse from line #19
+//Use math.random and for loop with parameters from user to create the password.  For loop should run a number of times equal to the length of characters chosen for this password.
+
+//THEN CALL the function
 
 // Write password to the #password input
 //Functions "Encapsulate" a task (they combine many instructions into a single line of code). 
@@ -51,11 +61,13 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  //need to connect this to the HTML  Looking for #password
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button in HTML
 
 generateBtn.addEventListener("click", writePassword);
+
